289fea9b73878e1c1af62da7a3fb91992dd4480a
Maree Palmer <mpa588@uclive.ac.nz>
2020-10-19 17:02:58 +1300

    Added about screen in navigation

--- app/src/main/java/com/lovehack/carecompanion/MainActivity.kt
+++ app/src/main/java/com/lovehack/carecompanion/MainActivity.kt
@@ -35,7 +35,8 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_reminders, R.id.nav_work_tracker, R.id.nav_puppies, R.id.nav_help), drawerLayout)
+            R.id.nav_reminders, R.id.nav_work_tracker, R.id.nav_puppies, R.id.nav_help,
+            R.id.nav_about), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
